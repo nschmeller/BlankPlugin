bdf60b9925091c183addd367614b9cadf742289f
EC2 Default User <ec2-user@ip-172-31-20-51.ec2.internal>
2020-12-16 02:45:41 +0000

    initial commit
    followed tutorial at https://www.spigotmc.org/wiki/creating-a-blank-spigot-plugin-in-vs-code/

---
+++ src/main/java/dev/nschmeller/spigot/blankplugin/App.java
@@ -0,0 +1,13 @@
+package dev.nschmeller.spigot.blankplugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
